502077a47aa740b14549a302d6a20d954c323259
Wenlong Zhang <longlong198@gmail.com>
2023-02-27 14:26:23 -0500

    Change the homepage name and description to fit my own experience

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,9 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Finite Element Method Developer",
+          "Researcher",
+          "Enthusiatic Learner",
         ],
         autoStart: true,
         loop: true,
