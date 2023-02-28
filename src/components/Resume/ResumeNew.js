9053463e9d81a93f56febe36a8a2a25eaa22310d
Wenlong Zhang <wenlongzhang@wenlongs-air.lan>
2023-02-27 22:21:11 -0500

    Update the resume page so that resume is scrollable in Y direction

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/../Assets/WenlongResume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
@@ -13,6 +13,13 @@ const resumeLink =
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [numPages, setNumPages] = useState(null);
+
+  function onDocumentLoadSuccess ({numPages}) {
+    setNumPages(numPages);
+    setPageNumber(1);
+  }
 
   useEffect(() => {
     setWidth(window.innerWidth);
@@ -35,8 +42,29 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          <Document 
+            file={pdf} 
+            onLoadSuccess = {onDocumentLoadSuccess}
+            className="d-flex justify-content-center"
+            pageLayout="oneColumn"
+          >
+            <div
+              style={
+                { maxHeight: '1000px',
+                  overflowY: 'scroll'
+                }
+              }
+            >
+              {Array.from(new Array(numPages), (el,index) => (
+                <Page 
+                  key={`page_${index+1}`} 
+                  pageNumber={index+1} 
+                  scale={width > 786 ? 1.7 : 0.6}
+                />
+              ))}              
+            </div>
+
+            {/* <Page pageNumber={pageNumber} scale={width > 786 ? 1.7 : 0.6} /> */}
           </Document>
         </Row>
 
